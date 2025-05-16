fc60c57f59d076ade45c0b1f5be6d4168abf94bc
harish876 <harishgokul01@gmail.com>
2025-05-15 17:48:56 -0700

    Enhance error handling in ResilientPythonCache and add main synchronization script

---
+++ test.py
@@ -0,0 +1,42 @@
+import asyncio
+from resilient_python_cache import ResilientPythonCache, MongoConfig, ResilientDBConfig
+
+async def main():
+    mongo_config = MongoConfig(
+        uri="mongodb+srv://harishgokul01:PMMemqOq8SPJFhkj@cluster0.rxtgmuk.mongodb.net/",
+        db_name="resdb-cache",
+        collection_name="resdb-collection"
+    )
+        
+    resilient_db_config = ResilientDBConfig(
+        base_url="resilientdb://crow.resilientdb.com",
+        http_secure=True,
+        ws_secure=True
+    )
+
+    cache = ResilientPythonCache(mongo_config, resilient_db_config)
+
+    cache.on("connected", lambda: print("WebSocket connected."))
+    cache.on("data", lambda new_blocks: print("Received new blocks:", new_blocks))
+    cache.on("error", lambda error: print("Error:", error))
+    cache.on("closed", lambda: print("Connection closed."))
+
+    try:
+        await cache.initialize()
+        print("Synchronization initialized.")
+
+        try:
+            await asyncio.Future()  # Run indefinitely
+        except asyncio.CancelledError:
+            pass
+
+    except Exception as error:
+        print("Error during sync initialization:", error)
+    finally:
+        await cache.close()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Interrupted by user")
